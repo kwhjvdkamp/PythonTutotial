2ba7d6e831841f5fc6fb1429a37e679ea5c97668
kwhjvdkamp <info@van-der-kamp.nl>
2019-09-10 11:12:04 +0200

    modifications

--- dict_to_array.py
+++ dict_to_array.py
@@ -1,4 +1,4 @@
-# Covering 3 ways to write modular code with Python: 
+# Covering 3 ways to write modular code with Python:
 # > packages        e.g. pandas package
 # > classes, and    e.g. df class
 # > methods         e.g. plot method
@@ -7,7 +7,7 @@
 import pandas as pd
 
 # Create some example data
-data = {'x': [1, 2, 3, 4], 
+data = {'x': [1, 2, 3, 4],
         'y': [20.1, 62.5, 34.8, 42.7]}
 
 # Create a dataframe CLASS object
@@ -16,5 +16,7 @@ df = pd.DataFrame(data)
 # terminal test
 # print(df)
 
-# Use the plot METHOD (creates an .exe file at C:/Users/info/AppData/Local/Programs/Python/Python37/python.exe )
+# Use the plot METHOD, it creates an .exe file at
+#  C:/Users/info/AppData/Local/Programs/Python/Python37/python.exe
+
 # df.plot('x', 'y')
