c67684cf85b133a7697b56bc04e021f4b8852101
kwhjvdkamp <info@van-der-kamp.nl>
2020-08-14 21:48:51 +0200

    wip

--- statistical-thinking/ecdf_versicolor_petal_length.py
+++ statistical-thinking/ecdf_versicolor_petal_length.py
@@ -1,18 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm
+import pandas as pd
+import seaborn as sns
+
 # Import learning dataset from scikit-learn.org
 from def_iris import iris
 
 # (E)CDF (Empirical) Cumulative Distribution Function
 from def_ecdf import ecdf
 
-# # Compute ECDF for versicolor data: x_vers, y_vers
-# x_vers, y_vers = ecdf(versicolor_petal_length)
+iris = iris()
+data = iris["data"][:, :4]
+print(data)
+target = iris["target"]
+print(target)
+target_names = iris["target_names"]
+print(target_names)
+df = pd.DataFrame(data, columns = ["sepal length (cm)", "sepal width (cm)", "petal length (cm)", "petal width (cm)"])
+print(df.head())
+print(df.values[0:2, 0:4])
 
-# # Generate plot
-# _ = plt.plot(x_vers, y_vers, marker='.', linestyle='none')
+versicolor_petal_length = df.iloc[0:-1,0:1].values
 
-# # Label the axes
-# _ = plt.xlabel('species')
-# _ = plt.ylabel('ECDF')
+# Compute ECDF for versicolor data: x_vers, y_vers
+x_vers, y_vers = ecdf(versicolor_petal_length)
 
-# # Display the plot
-# plt.show()
+# Generate plot
+_ = plt.plot(x_vers, y_vers, marker='.', linestyle='none')
+
+# Label the axes
+_ = plt.xlabel('species')
+_ = plt.ylabel('ECDF')
+
+# Display the plot
+plt.show()
