509b4dec0d533c4668a2bdbb00dcfa9c6e9a2e51
kwhjvdkamp <info@van-der-kamp.nl>
2020-08-16 19:57:16 +0200

    ...

--- statistical-thinking/def_iris.py
+++ statistical-thinking/def_iris.py
@@ -23,8 +23,12 @@ def iris():
     target_names = iris["target_names"]
     # print('dataframe_key: \'target_names\':\r\n', target_names)
 
+    sepal_l = "sepal length (cm)"
+    sepal_w = "sepal width (cm)"
+    petal_l = "petal length (cm)"
+    petal_w = "petal width (cm)"
     df = pd \
-        .DataFrame(data, columns = ["sepal length (cm)", "sepal width (cm)", "petal length (cm)", "petal width (cm)"])
+        .DataFrame(data, columns = [sepal_l, sepal_w, petal_l, petal_w])
     # print('dataframe: \'df.head()\':\r\n', df.head())
     # print('dataframe: \'statistics\': ', df.describe())
 
