1560f2bb4746d36f98ecac0f08abf8b13af98691
kwhjvdkamp <info@van-der-kamp.nl>
2020-11-22 17:27:30 +0100

    wip

--- misc/part_4.py
+++ misc/part_4.py
@@ -25,3 +25,11 @@ print('[',i,'] > ', text)
 i += 1
 text = '{min} <= {number} <= {max}'.format(min = 1, max = 10, number = 5)
 print('[',i,'] > ', text)
+
+# 03 Interate thru a list with 'for'
+i += 1
+n = 0
+names = ['Jan','Kees','Koen','Piet','Koen']
+print('[',i,'] > The enumerate object yields pairs containing a count \n        (from start, which defaults to zero)\n        and a value yielded by the iterable argument.\n        \'Enumerate(...)\' is useful for obtaining an indexed list:')
+for index, name in enumerate(names) :
+    print('[',i,'] > ', '{0} {1}'.format(index, name))
