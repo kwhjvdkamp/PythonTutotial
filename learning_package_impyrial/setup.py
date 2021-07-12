03211c438c4097127fa4895e062cd3ef6321036b
kwhjvdkamp <info@van-der-kamp.nl>
2021-07-12 15:35:43 +0200

    wip

--- learning_package_impyrial/setup.py
+++ learning_package_impyrial/setup.py
@@ -1,5 +1,6 @@
 # Import required functions
 from setuptools import find_packages, setup
+
 # Call setup function
 setup(
     author="Koen van der Kamp",
@@ -8,5 +9,9 @@ setup(
     packages=find_packages(include=['learning_package_impyrial','learning_package_impyrial.*']),
     # version number = (major number) . (minor number) . (patch number)
     version="0.1.0",
+
+    install_requires=['pandas', 'scipy', 'matplotlib'],
+
+    python_requires='>=3.9.5',
 
 )
