fc0b85b5c84323fb6a932bc7938cabb94b338e4a
kwhjvdkamp <info@van-der-kamp.nl>
2021-07-12 10:50:02 +0200

    wip

--- learning_package_impyrial/__init__.py
+++ learning_package_impyrial/__init__.py
@@ -1,15 +1,15 @@
 """
 
 [Absolute Imports]
-Statement 'from <package_name> import <package>' says
-> explicit importing <sub-package> at this level
+Statement 'from <package_name>.<subpackage_name> import <package>' says
+> explicit importing <package> at this level
 
 or
 
 [Relative Imports]
 Statement 'from . import <package>' says
 > start searching from the current file
-> from '.' the 'current' directory import <sub-package>
+> from '.' the 'current' directory import <package>
 
 
 [According documentation style 'impyrial' at PACKAGE level]
