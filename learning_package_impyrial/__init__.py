9ce9920b5e5d46e779e90ee07764f66b3d12ef6a
kwhjvdkamp <info@van-der-kamp.nl>
2021-07-12 10:44:14 +0200

    wip

---
+++ learning_package_impyrial/__init__.py
@@ -0,0 +1,17 @@
+"""
+
+[Absolute Imports]
+Statement 'from <package_name> import <package>' says
+> explicit importing <sub-package> at this level
+
+or
+
+[Relative Imports]
+Statement 'from . import <package>' says
+> start searching from the current file
+> from '.' the 'current' directory import <sub-package>
+
+
+[According documentation style 'impyrial' at PACKAGE level]
+
+"""
---
+++ learning_package_numpy/__init__.py
@@ -0,0 +1,17 @@
+"""
+
+[Absolute Imports]
+
+    # form <package_name> import <package> says
+    # > explicit importing <sub-package> at this level
+
+    # or
+
+[Relative Imports] : text
+    'from . import <package>' says
+    > start searching from the current file
+    > from '.' the 'current' directory import <sub-package>
+
+[According documentation style 'numpydoc' at PACKAGE level]
+
+"""
