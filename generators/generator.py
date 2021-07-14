716e8f0ceee703503938775d1d6c1607ce27bafe
kwhjvdkamp <info@van-der-kamp.nl>
2021-07-14 09:24:56 +0200

    wip

--- generators/generator.py
+++ generators/generator.py
@@ -1,3 +1,28 @@
+#	Yield							|	Return
+#	=========================================================================
+# 1	Yield is generally used to 		|	Return is generally used for ending
+#	convert a regular Python 		|	the execution and “returns”
+#	function into a generator. 		|	result to the caller statement.
+#	-------------------------------------------------------------------------
+# 2	It replace the return of a 		|	It exits a function and handing back
+#	function to suspend its 		|	a value to its caller.
+#	execution without destroying	|
+#	local variables.				|
+#	-------------------------------------------------------------------------
+# 3	It is used when the generator	|	It is used when a function.
+#	returns an intermediate result	|	is ready to send a value.
+#	to the caller.
+#	-------------------------------------------------------------------------
+# 4	Code written after yield 		|	Code written after return statement
+# 	statement executes in next 		|	won't be executed.
+#	function call.					|
+#	-------------------------------------------------------------------------
+# 5	It can run multiple times.		|	It only runs single time.
+# 6	'yield' functions executes 		|	Every function calls run the
+#	from the last state from where 	|	'return' function from the start.
+#	the function get paused.		|
+#	-------------------------------------------------------------------------
+
 # Rewrite func1() as a generator comprehension
 def func1(n):
   for i in range(0, n):
--- generators/generator_shift-in-string.py
+++ generators/generator_shift-in-string.py
@@ -1,14 +1,36 @@
-# 'yield' is used in Python generators 
-# A generator function is defined like a normal function, 
-# but whenever it needs to generate a value, it does so with 
-# the 'yield' keyword rather than return. 
-# If the body of a def contains yield, the function automatically 
-# becomes a generator function
+# 'yield' is used in Python generators
+# A generator function is defined like a normal function, but whenever it needs
+# to generate a value, it does so with the 'yield' keyword rather than return.
+# If the body of a def contains yield, it automatically becomes a generator function
 def shift_string(string, shift):
     len_string = len(string)
     print("Length of ", string, "=" , len(string))
     # Define a for-loop with 'yield' statement
     for idx in range(0, len_string):
+        #	Yield							|	Return
+        #	=========================================================================
+        # 1	Yield is generally used to 		|	Return is generally used for ending
+        #	convert a regular Python 		|	the execution and “returns”
+        #	function into a generator. 		|	result to the caller statement.
+        #	-------------------------------------------------------------------------
+        # 2	It replace the return of a 		|	It exits a function and handing back
+        #	function to suspend its 		|	a value to its caller.
+        #	execution without destroying	|
+        #	local variables.				|
+        #	-------------------------------------------------------------------------
+        # 3	It is used when the generator	|	It is used when a function.
+        #	returns an intermediate result	|	is ready to send a value.
+        #	to the caller.
+        #	-------------------------------------------------------------------------
+        # 4	Code written after yield 		|	Code written after return statement
+        # 	statement executes in next 		|	won't be executed.
+        #	function call.					|
+        #	-------------------------------------------------------------------------
+        # 5	It can run multiple times.		|	It only runs single time.
+        # 6	'yield' functions executes 		|	Every function calls run the
+        #	from the last state from where 	|	'return' function from the start.
+        #	the function get paused.		|
+        #	-------------------------------------------------------------------------
         yield string[(idx - shift) % len_string]
 
 # Create a generator
