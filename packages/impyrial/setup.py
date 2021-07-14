218bdf1dbc8ec6ef79e3b989cf6f9abfb2e520bc
kwhjvdkamp <info@van-der-kamp.nl>
2021-07-14 13:35:38 +0200

    wip

--- packages/impyrial/setup.py
+++ packages/impyrial/setup.py
@@ -1,13 +1,41 @@
 # Import required functions
 from setuptools import find_packages, setup
 
-# Call setup function
+#Example
+# setup(
+#       name="univariate_linear_regression",
+#       version="0.1.0",
+#       description="Univariate linear regression of housing price against housing area",
+#       author="Dibya Chakravorty",
+#       packages=find_packages("src"),
+#       package_dir={"": "src"},
+#       author_email="dibyachakravorty@gmail.com",
+#       install_requires=["jupyter==1.0.0",
+#                         "matplotlib==3.1.1",
+#                         "numpy==1.17.3",
+#                         "pytest==5.2.2",
+#                         "pytest-mpl==0.10",
+#                         "pytest-mock==1.11.2",
+#                         "scipy==1.3.1",
+#                         ],
+#       )
+
 setup(
-    author="Koen van der Kamp",
-    description="Learning package - Conversions",
     name="impyrial",
-    packages=find_packages(include=['impyrial', 'impyrial.*']),
     version="0.1.0",
-    install_requires=['pandas', 'scipy', 'matplotlib'],
+    description="Learning package - Conversions",
+    author="Koen van der Kamp",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    author_email="info@van-der-kamp.nl",
+    install_requires=[
+        "jupyter==1.0.0",
+        "matplotlib==3.1.1",
+        "numpy==1.17.3",
+        "pytest==5.2.2",
+        "pytest-mpl==0.10",
+        "pytest-mock==1.11.2",
+        "scipy==1.3.1",
+    ],
     python_requires='>=3.9.5',
 )
