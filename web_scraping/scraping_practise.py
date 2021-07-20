e32031b26a8ee93d9b78b10ec3ce80f0dfe85d1b
K.W.H.J. van der Kamp <kwhj.vanderkamp@gmail.com>
2021-07-20 19:57:55 +0200

    wip

--- web_scraping/scraping_practise.py
+++ web_scraping/scraping_practise.py
@@ -20,9 +20,9 @@ sel = Selector(text=html)
 # Create a Selector selecting html as the HTML document
 sel = Selector( text=html )
 # Create a SelectorList of all div elements in the HTML document
-divs = sel.xpath( "//div" )
-print(divs)
-print(divs[2])
+diversions = sel.xpath( "//div" )
+print(diversions)
+print(diversions[2])
 # OUTPUT
 # [
 #   <Selector xpath='//div' data='<div>Div 1: <p>paragraph 1</p></div>'>,
@@ -31,7 +31,7 @@ print(divs[2])
 #   <Selector xpath='//div' data='<div>Div 4: <p>paragraph 7</p></div>'>,
 #   <Selector xpath='//div' data='<div>Div 5: <p>paragraph 8</p></div>'>
 # ]
-print(divs[2].xpath('./*'))
+print(diversions[2].xpath('./*'))
 # OUTPUT
 # <Selector xpath='//div' data='<div>Div 3: <p>paragraph 4</p> <p>par...'>
 # [
@@ -39,13 +39,13 @@ print(divs[2].xpath('./*'))
 #   <Selector xpath='./*' data='<p>paragraph 5</p>'>,
 #   <Selector xpath='./*' data='<p>paragraph 6</p>'>
 # ]
-print(len( divs[2].xpath('./*') ))
+print(len( diversions[2].xpath('./*') ))
 # OUTPUT
 # 3
-print(len( divs[2] ))
+print(len( diversions[2] ))
 # OUTPUT
 # TypeError: object of type 'Selector' has no len()
-# The code divs[2] is one element from a SelectorList, and therefore is a Selector (not another SelectorList)
+# The code diversions[2] is one element from a SelectorList, and therefore is a Selector (not another SelectorList)
 
 
 # url = 'https://assets.datacamp.com/production/repositories/2560/datasets/19a0a26daa8d9db1d920b5d5607c19d6d8094b3b/all_short'
